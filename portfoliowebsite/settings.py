6d05ff618acb8c3283980f485473ae90cfb5651e
Trinh Mai <maitrinhdnc@gmail.com>
2021-08-17 08:16:42 +0700

    “edit”

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z8f$s((3(b5-&to4v#o1c^+*_&mjnon*wa-k-7a4e@(4l@ly^5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://trinh-portfoliowebsite.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
